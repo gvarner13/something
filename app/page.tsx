594f76a3a0fd3b833be9af21d4ac9d62fb7353af
Gary Varner <garysarahvarner@gmail.com>
2024-02-16 21:27:20 -0500

    Added layout for waitlist form

--- app/page.tsx
+++ app/page.tsx
@@ -148,30 +148,32 @@ export default function Home() {
           <LogosNextjsIcon className="text-6xl" />
           <LogosSupabaseIcon className="text-6xl" />
         </div>
-        <div className="max-w-xl lg:max-w-lg">
-          <p className="mt-4 text-lg leading-8 text-gray-300">
-            Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing
-            velit quis. Duis tempor incididunt dolore.
-          </p>
-          <div className="mt-6 flex max-w-md gap-x-4">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <button
-              type="submit"
-              className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            >
-              Subscribe
-            </button>
+        <div className="flex justify-center text-center">
+          <div className="max-w-xl lg:max-w-lg">
+            <p className="mt-4 text-lg leading-8 text-gray-300">
+              Sound interesting? Sign up below for the waitlist and get updates
+              as this develops.
+            </p>
+            <div className="mt-6 flex max-w-md gap-x-4 mx-auto">
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                placeholder="Enter your email"
+              />
+              <button
+                type="submit"
+                className="flex-none rounded-md bg-[#FFA8C5] px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-[#FFBCD9] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
