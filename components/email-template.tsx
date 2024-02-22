6417d0822df8bc50754ed4d0b2634e5cc331a0bf
Gary Varner <garysarahvarner@gmail.com>
2024-02-21 19:40:07 -0600

    adding twitter to intro email

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -13,5 +13,9 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
       Thank you for interest in my template project. Stay tuned for more info as
       it progresses towards release.
     </p>
+    <p>
+      You can follow me on twitter for more often updates.{" "}
+      <a href="https://twitter.com/GSVarner">@GSVarner</a>
+    </p>
   </div>
 );
