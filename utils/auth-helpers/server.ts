731c0cce6bbb0ed9824495ec731a78b3946512e2
Gary Varner <garysarahvarner@gmail.com>
2024-03-05 22:34:18 -0500

    adding signout server action for supdabse auth

---
+++ utils/auth-helpers/server.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function SignOut() {
+  const supabase = createClient();
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/");
+}
